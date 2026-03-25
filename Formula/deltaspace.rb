5498eaec841fab4570b32bfa639c52a3231d068a
Jarmoco <jacopolanza05@gmail.com>
2026-03-25 19:28:10 +0100

    * update 0.1.3

--- Formula/deltaspace.rb
+++ Formula/deltaspace.rb
@@ -5,12 +5,12 @@ class Deltaspace < Formula
 
   # 1. Define URLs based on the Operating System
   if OS.mac?
-    url "https://github.com/Jarmoco/DeltaSpace/releases/download/v0.1.2/deltaspace_0.1.2_macos_aarch64.tar.gz"
-    sha256 "62790950ca14aad20174d05d4b576d81378dfff69f464c97f0ad7ee0a04da3af"
+    url "https://github.com/Jarmoco/DeltaSpace/releases/download/v0.1.3/deltaspace_0.1.3_macos_aarch64.tar.gz"
+    sha256 "ea106fb2b8e70694f55c2db787f1014960f0874ad2cbdb88142f4fec909ec0e6"
   elsif OS.linux?
     # Replace these with your actual Linux release URL and its SHA-256
-    url "https://github.com/Jarmoco/DeltaSpace/releases/download/v0.1.2/deltaspace_0.1.2_linux_x86_64.tar.gz"
-    sha256 "702a322376339c824d0e7aad2badd6ff7fbbe1b4a26c1cffae2e8f64f1cd1189"
+    url "https://github.com/Jarmoco/DeltaSpace/releases/download/v0.1.3/deltaspace_0.1.3_linux_x86_64.tar.gz"
+    sha256 "b516656d66d48c00c2f52edd81a2e43e629b5cf60fa6039aa1b8d73eced68e15"
   end
 
   def install
